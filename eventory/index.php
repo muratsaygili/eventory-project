264175b3666b9a426da7dc124ac869cd640017b5
Yunus Emre Erken <yunusemreerken@users.noreply.github.com>
2016-05-26 00:56:48 +0300

    Update index.php
    ana sayfa düzenlendi

--- eventory/index.php
+++ eventory/index.php
@@ -28,21 +28,141 @@ require_once "config.php";
         <!-- page content -->
             <div class="right_col" role="main">
               <div class="">
-                <div class="row">
-                  <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="x_panel" style="height:600px;">
-                      <div class="x_title">
-                        <h2>"slogan"</h2>
-                        <div class="clearfix"></div>
+                  <div class="row">
+                      <div class="col-md-12">
+                          <div class="x_panel">
+                              <div class="x_title">
+                                  <h2>Başlıca Etkinlikler </h2>
+
+                                  <div class="clearfix"></div>
+                              </div>
+                              <div class="x_content">
+
+                                  <div class="row">
+
+                                      <p><h4>Vitrindekiler</h4></p>
+
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/4.jpg" alt="image">
+                                                  <div class="mask">
+                                                      <p>Your Text</p>
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p>Snow and Ice Incoming for the South</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/4.jpg" alt="image">
+                                                  <div class="mask">
+                                                      <p>Your Text</p>
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p>Snow and Ice Incoming for the South</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/4.jpg" alt="image">
+                                                  <div class="mask">
+                                                      <p>Your Text</p>
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p>Snow and Ice Incoming for the South</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/4.jpg" alt="image">
+                                                  <div class="mask">
+                                                      <p>Your Text</p>
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p>Snow and Ice Incoming for the South</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/4.jpg" alt="image">
+                                                  <div class="mask">
+                                                      <p>Your Text</p>
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p>Snow and Ice Incoming for the South</p>
+                                              </div>
+                                          </div>
+                                      </div>
+
+
+                                      <div class="col-md-4">
+                                          <div class="thumbnail">
+                                              <div class="image view view-first">
+                                                  <img style="width: 100%; display: block;" src="images/yapim_asamasinda.png" alt="image">
+                                                  <div class="mask no-caption">
+                                                      <div class="tools tools-bottom">
+                                                          <a href="#"><i class="fa fa-link"></i></a>
+                                                          <a href="#"><i class="fa fa-pencil"></i></a>
+                                                          <a href="#"><i class="fa fa-times"></i></a>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                              <div class="caption">
+                                                  <p><strong>Image Name</strong>
+                                                  </p>
+                                                  <p>Snow and Ice Incoming</p>
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <div class="col-md-4"></div>
+                                      <div class="col-md-4"></div>
+                                      <div class="col-md-4" style="text-align: right">
+                                          <a  href="dahasi.php"><p style="font-size: larger" >Daha fazlası için Tıklamanız yeterli...</p> </a>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
-içerik
-
-
-
-
-                    </div>
                   </div>
-                </div>
               </div>
             </div>
         <!-- /page content -->
